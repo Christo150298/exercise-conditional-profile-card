d3dfc004418568771b9acc706ca65cf527b01c62
Christopher Álvarez <christopherjavier1998@gmail.com>
2023-02-08 21:18:35 +0000

    avances de proyecto

--- src/js/app.js
+++ src/js/app.js
@@ -28,15 +28,23 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
+  let name = variables.name ? `${variables.name}` : "Name";
+  let lastname = variables.lastname ? `${variables.lastname}` : "LastName";
+  let redesPosition =
+    variables.socialMediaPosition == "position-right"
+      ? `position-right`
+      : `position-left`;
+  let role = variables.role ? `${variables.role}` : "Role";
+  let city = variables.city ? `${variables.city}` : "City";
+  let country = variables.country ? `${variables.country}` : "Country";
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
+          ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${name} ${lastname}</h1>
+          <h2>${role}</h2>
+          <h3>${city} ${country}</h3>
+          <ul class="${redesPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
@@ -54,9 +62,11 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://images2.minutemediacdn.com/image/upload/c_crop,w_3543,h_1992,x_0,y_186/c_fill,w_1440,ar_16:9,f_auto,q_auto,g_auto/images/GettyImages/mmsport/90min_es_international_web/01fm50ap4s7s95gd0bjz.jpg",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://www.debate.com.uy/__export/1670853449210/sites/debate/img/2022/12/12/qux_mirx_bobo.jpg_969990731.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
